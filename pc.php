e4dfbd3de7b495fb00b70f9178e60f51b0913e5b
Sandani Wimalaweera <144089098+sandaniwimalaweera@users.noreply.github.com>
2024-08-23 19:00:19 +0530

    Update pc.php

--- pc.php
+++ pc.php
@@ -1,15 +1,4 @@
 <?php require_once('connection/connection.php'); ?>
-<?php 
-$username='admin';
-$password='1234';
-$hashed_password=sha1($password);
- 
-//$query="INSERT INTO login(username,password,date)VALUES('{$user}','{$hashed_password}')";
 
-$query="INSERT INTO admin (username,password) VALUES('$username','$hashed_password') ";
-
-
-$result=mysqli_query($connection,$query);
-?>
 </body>
 </html>
